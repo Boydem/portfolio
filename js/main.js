52f103436e819e6acb41ae4bedcca59870c9584f
Boydem <noyemdahan@gmail.com>
2022-12-06 00:41:50 +0200

    starteed working on contact form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
-console.log('Starting up');
+'use strict'
+
+$(onInit)
+
+function onInit() {
+  renderProjs()
+}
+
+function renderProjs() {
+  const projects = getProjsToDisplay()
+  var strHTMLs = projects.map(proj =>
+    `<div data-proj-id="${proj.id}" class="col-md-4 col-sm-6 portfolio-item">
+        <div class="content-wrapper">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid rounded" src="img/portfolio/${proj.id}-tb.png" alt="">
+          </a>
+          <div class="portfolio-caption">
+            <h5>${proj.name}</h5>
+            <p class="text-muted">${proj.type}</p>
+          </div>
+        </div>
+      </div>
+    `
+  )
+  $('.projs-container').html(strHTMLs)
+  $('.portfolio-item').on('click', renderProjForModal)
+}
+renderProjForModal()
+
+function renderProjForModal() {
+  const projects = getProjsToDisplay()
+  var currProj = projects.find(proj => proj.id === this.dataset['projId'])
+  var strHTML = `
+                <!-- Project Details Go Here -->
+                <h2>${currProj.name}</h2>
+                <p class="item-intro text-muted">${currProj.title}</p>
+                <img class="img-fluid d-block mx-auto" src="img/portfolio/${currProj.id}.png" alt="">
+                <p>${currProj.desc}</p>
+                <ul class="list-inline">
+                  <li>Date: January 2017</li>
+                  <li>Client: Threads</li>
+                  <li>Category: Illustration</li>
+                </ul>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+  `
+  $('.modal-body').html(strHTML)
+}
---
+++ js/projs.service.js
@@ -0,0 +1,61 @@
+'use strict'
+
+var gProjects = [{
+    id: "minesweeper",
+    name: "Minesweeper",
+    title: "Better push those boxes",
+    type: 'Game',
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "boydem.github.io/minesweeper",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "Game", "UX/UI", "Vanilla Javascript", "Sprint Project", "MVC"],
+}, {
+    id: "pacman",
+    name: "Pacman",
+    title: "Better push those boxes",
+    type: 'Game',
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "boydem.github.io/minesweeper",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "Game", "UX/UI", "Vanilla Javascript", "Sprint Project"],
+}, {
+    id: "touch",
+    name: "Touch Nums",
+    title: "Better push those boxes",
+    type: 'Game',
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "boydem.github.io/minesweeper",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "Game", "UX/UI", "Vanilla Javascript", "Sprint Project"],
+}, {
+    id: "books",
+    name: "Books",
+    title: "Better push those boxes",
+    type: 'App',
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "boydem.github.io/minesweeper",
+    publishedAt: 1448693940000,
+    labels: ["App", "Filters", "URLQueryParams", "Smart Refresh", "UX/UI", "Vanilla Javascript", "Dashboard", "MVC"],
+}, {
+    id: "login",
+    name: "Login",
+    title: "Better push those boxes",
+    type: 'App',
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "boydem.github.io/minesweeper",
+    publishedAt: 1448693940000,
+    labels: ["HTML", "CSS", "Vanilla Javascript", "UX/UI", "MVC", "localStorage", "Admin Panel", "Hidden Content"],
+}, {
+    id: "in-picture",
+    name: "Image Guess",
+    type: 'Game',
+    title: "Better push those boxes",
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "boydem.github.io/minesweeper",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "Game", "UX/UI", "Vanilla Javascript", "Sprint Project"],
+}]
+
+function getProjsToDisplay() {
+    return gProjects
+}
